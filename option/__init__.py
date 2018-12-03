37c954e6e74273d48649b3236bc881a1286107d6
Peijun Ma <peijun.ma@protonmail.com>
2018-12-03 18:19:23 -0500

    Bump version

--- option/__init__.py
+++ option/__init__.py
@@ -30,5 +30,5 @@
 from .option_ import NONE, Option, Some, maybe
 from .result import Err, Ok, Result
 
-__version__ = '0.2.5'
+__version__ = '0.3.0'
 __all__ = ['NONE', 'Option', 'Some', 'maybe', 'Result', 'Ok', 'Err']
