3c8530a4c8a4e2bd36e3b486618503bef9872eac
Peijun Ma <peijun.ma@protonmail.com>
2021-02-13 15:11:32 -0500

    Disable pip search
    https://github.com/pypa/pip/issues/5216

--- publish.py
+++ publish.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python3
 
-import re
 from os import getenv
-from subprocess import PIPE, run
+from subprocess import run
 from sys import argv
 
 from option import __version__
 
-search = run(['pip', 'search', 'option'], stdout=PIPE)
-stdout = search.stdout.decode()
-print(stdout)
-
-regex = re.compile(r'option\s+\((\d+\.\d+\.\d+)\)')
-cur_version = regex.match(stdout).groups(1)[0].split('.')
 new_version = __version__.split('.')
-print(f'Current version: {cur_version}, New version: {new_version}')
 
-if new_version > cur_version and getenv('UPLOAD_COV') == 'true':
+if getenv('UPLOAD_COV') == 'true':
     _, pypi_user, pypi_pass = argv
     run(['poetry', 'publish', '--build', '-u', pypi_user, '-p', pypi_pass])
